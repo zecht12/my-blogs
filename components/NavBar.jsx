3fb5a4f243434066e6807ad6c80ed34583b5a0ec
zecht12 <gilangprimae13@gmail.com>
2023-01-31 12:17:16 +0700

    membenahi tampilan

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -20,12 +20,12 @@ const changeColor = () => {
     }
 }
 
-
+window.addEventListener('scroll', changeColor);
 
 return (
     <div id='navbar' className={color ? "fixed w-full h-20 bg-blue-900 shadow-xl z-[100]" : " fixed w-full h-20 shadow-xl z-[100]"}>
         <div className='flex justify-between items-center w-full h-full px-16 2xl:px-16 '>
-            <Image src=''lt="" width='80' height='80' />
+            <Image src='' alt="" width='80' height='80' />
         <div>
             <ul className='hidden md:flex'>
                 <Link href='/' className='ml-10 text-base uppercase text-black font-bold  hover:text-red-500' >
